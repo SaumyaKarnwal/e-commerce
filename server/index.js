54c6a4dd4d41eff49e9ee859856ffbf5959dc343
SaumyaKarnwal <sau.kar.03@gmail.com>
2023-10-10 13:46:16 +0530

    login-setup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import Connection from './database/db.js';
 import dotenv from 'dotenv';
 import DefaultData from './default.js';
+import cors from 'cors';
+import Routes from './routes/route.js';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 const app = express();
@@ -11,6 +14,11 @@ const username  = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 Connection(username , password);
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/', Routes);
 
 app.listen(PORT, ()=> console.log('Server is up and running'));
 
